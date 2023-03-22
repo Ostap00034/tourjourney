520e7e905fd08f3a9154e70e0905bee750f5b016
Ostap Chichiginarov <ostap00034@gmail.com>
2023-03-22 14:35:29 +0900

    add tailwindcss and basic routing between screens

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Header = () => {
+	return (
+		<div>
+			<Nav />
+			Header
+		</div>
+	)
+}
+
+export default Header
---
+++ src/components/Header/Nav.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Nav = () => {
+	return <div>Nav sdfasdfdsf</div>
+}
+
+export default Nav
